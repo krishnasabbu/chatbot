23b9b03ba8c28ca8ea3c408264e45a12924265e7
krishnasabbu <krishna.sabbu@gmail.com>
2025-07-25 12:13:15 +0530

    Update layout.tsx

--- chat-app/app/layout.tsx
+++ chat-app/app/layout.tsx
@@ -4,9 +4,9 @@ import { GeistMono } from 'geist/font/mono'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.dev',
+  title: 'ChatBot',
+  description: 'Created with cortex'
+  generator: 'cortex',
 }
 
 export default function RootLayout({
